77da1706fdd5d76a929109e5af5aa5da154eed6b
Glacial <glacial@gmail.com>
2019-05-15 22:05:03 -0400

    Add header info

--- back-end/dist/2019-ecomar/components/page-header.php
+++ back-end/dist/2019-ecomar/components/page-header.php
@@ -2,12 +2,11 @@
     <div class="container">
         <div class="row align-items-center">
             <div class="page-header-brand col-auto">
-                <a class="page-header-logo" href="index.html"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/brand-logo.svg" alt=""></a>
-                <h1 class="page-header-title">Ecomar</h1>
+                <a class="page-header-logo" href="/"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/brand-logo.svg" alt=""></a>
+                <h1 class="page-header-title"><?php echo get_bloginfo( 'name' ); ?></h1>
             </div>
             <div class="col d-none d-md-none d-lg-block">
-                <p class="page-header-description">Fundo de Desenvolvimento das Actividades Económicas do Mar e Águas
-                    Interiores</p>
+                <p class="page-header-description"><?php echo get_bloginfo( 'description' ); ?></p>
             </div>
             <div class="page-header-search col-2 d-none d-md-none d-lg-block">
             <?php get_template_part('components/search-form'); ?>
