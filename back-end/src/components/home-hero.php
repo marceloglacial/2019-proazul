4dfbccdb3efbd84eef2259da3be3d259f441ee7b
Glacial <glacial@gmail.com>
2019-05-21 00:47:13 -0400

    Add link to hero

--- back-end/src/components/home-hero.php
+++ back-end/src/components/home-hero.php
@@ -3,7 +3,7 @@
 ?>
 <div class="jumbotron jumbotron-fluid" style="background-image: url('<?php echo $image['url']; ?>')">
     <div class="hero-text container d-flex flex-column justify-content-center justify-content-lg-end align-items-end">
-        <a href="single.html" class="hero-link">
+        <a href="<?php the_field('link'); ?>" class="hero-link">
             <h1 class="hero-title"><?php the_field('titulo'); ?></h1>
             <p class="hero-lead lead"><?php the_field('subtitulo'); ?></p>
         </a>
