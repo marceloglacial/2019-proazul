1476a5647dc1a880da1bb5171a5e1e88ee4f409f
Glacial <glacial@gmail.com>
2019-05-21 23:16:33 -0400

    Remove all featured image reference

--- back-end/src/components/card-big.php
+++ back-end/src/components/card-big.php
@@ -1,11 +1,7 @@
 <div class="card card-big">
   <a href="<?php echo get_permalink($post_id); ?>" class="card-link">
       <div class="card-img">
-      <?php if (get_the_post_thumbnail($post_id)) { ?>
-        <img src="<?php echo  get_the_post_thumbnail_url($post_id); ?>" alt="" class="card-img-top">
-      <?php } else { ?>
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/placeholder.png" alt="" class="card-img-top">
-      <?php } ?>
+        <img src="<?php echo catch_that_image($post_id); ?>" alt="" class="card-img-top">
       </div>
       <div class="card-body">
         <h5 class="card-title"><?php echo mb_strimwidth(get_the_title($post_id), 0, 40, '...'); ?></h5>
--- back-end/src/components/page-list-item.php
+++ back-end/src/components/page-list-item.php
@@ -1,10 +1,6 @@
 <section class="category-list-item row">
     <div class="category-list-item-header col-12 col-lg-4">
-        <?php if (get_the_post_thumbnail($post_id)) { ?>
-            <?php the_post_thumbnail(); ?>
-        <?php } else { ?>
-            <img src="<?php echo catch_that_image($post_id); ?>" alt="">
-        <?php } ?>
+        <img src="<?php echo catch_that_image($post_id); ?>" alt="">
     </div>
     <div class="category-list-item-lead col-12 col-lg">
         <h3 class="category-list-item-title"><?php the_title() ?></h3>
