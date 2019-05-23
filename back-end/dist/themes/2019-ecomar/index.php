9c996c88f653a4d1b10be38596a12999503cd965
Glacial <glacial@gmail.com>
2019-05-23 13:40:33 -0400

    Add default theme

--- back-end/dist/themes/2019-ecomar/index.php
+++ back-end/dist/themes/2019-ecomar/index.php
@@ -1,12 +1,24 @@
-<?php /* Template Name: Home Page */ ?>
 <?php get_header() ?>
-
 <?php get_template_part('components/page-header'); ?>
+<main class="container page page-search">
+    <header class="page-header">
+        <h1 class="page-title">Resultados da busca</h1>
+    </header>
+    <section class="category-list">
+        <?php if ( have_posts() ) : ?>
+            <?php while ( have_posts() ) : the_post();  ?>
+                <?php get_template_part('components/page-list-item'); ?>
+        <?php endwhile; ?>
 
-<?php get_template_part('components/home-hero'); ?>
+        <div class="category-list-nav">
+            <div class="category-list-nav-item nav-next"><?php next_posts_link( '&#8592; Mais antigos' ); ?></div>
+            <div class="category-list-nav-item nav-previous"><?php previous_posts_link( 'Mais recentes &#8594; ' ); ?></div>
+        </div>
 
-<?php get_template_part('components/home-content'); ?>
-
+        <?php else : ?>
+            <p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+        <?php endif; ?>
+    </section>
+</main>
 <?php get_template_part('components/page-footer'); ?>
-
 <?php get_footer() ?>
