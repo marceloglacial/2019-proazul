1335b2c37121debce874df328b2fabd382bd7638
Glacial <glacial@gmail.com>
2019-05-23 19:34:26 -0400

    add tags

--- back-end/src/components/page-list-item.php
+++ back-end/src/components/page-list-item.php
@@ -3,6 +3,22 @@
         <img src="<?php echo catch_that_image($post_id); ?>" alt="">
     </div>
     <div class="category-list-item-lead col-12 col-lg">
+        <div class="category-list-item-tags container-fluid">
+            <ul class="category-list-item-tags-list row">
+                <?php 
+                    $post_tags = get_the_tags();
+                    if ( $post_tags ) {
+                        foreach( $post_tags as $tag ) { 
+                ?>
+
+                <li class="category-list-item-tags-list-item col"><?php echo $tag->name; ?></li>
+                
+                <?php 
+                        }
+                    }
+                ?>
+                </ul>
+        </div>
         <h3 class="category-list-item-title"><?php the_title() ?></h3>
         <p class="category-list-item-description"><?php echo mb_strimwidth(get_the_excerpt($post_id), 0, 370, ' ...'); ?></p>
         <p>
