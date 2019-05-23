0a7e6aa3d7e2d63c692227845dac668c2b873860
Glacial <glacial@gmail.com>
2019-05-23 15:02:27 -0400

    Add 404 page

---
+++ back-end/dist/themes/2019-ecomar/404.php
@@ -0,0 +1,12 @@
+<?php get_header() ?>
+<?php get_template_part('components/page-header'); ?>
+<main class="container page page-search">
+    <header class="page-header">
+        <h1 class="page-title">Página não encontrada.</h1>
+    </header>
+    <section class="category-list">
+        <p>Desculpe, mas não encontramos a página que você estava procurando. <br /> Talvez você encontre algo usando nossa ferramenta de busca no canto superior direito.</p>
+    </section>
+</main>
+<?php get_template_part('components/page-footer'); ?>
+<?php get_footer() ?>
